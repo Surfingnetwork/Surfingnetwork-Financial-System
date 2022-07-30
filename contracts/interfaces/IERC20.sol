b9461c14b27607ee810069b65a08203fef9c45fa
Surfingnetwork <103472119+Surfingnetwork@users.noreply.github.com>
2022-07-30 16:27:39 +0800

    Update IERC20.sol

--- contracts/interfaces/IERC20.sol
+++ contracts/interfaces/IERC20.sol
@@ -6,7 +6,6 @@ interface IERC20 {
 
     function transfer(address recipient, uint256 amount) external returns (bool);
 
-
     function allowance(address owner, address spender) external view returns (uint256);
 
 
