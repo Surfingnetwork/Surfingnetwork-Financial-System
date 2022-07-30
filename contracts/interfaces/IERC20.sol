85e5c66dd3aecfc3de7c4330851e6a70bec22604
Surfingnetwork <103472119+Surfingnetwork@users.noreply.github.com>
2022-07-30 16:27:06 +0800

    Create IERC20.sol

--- contracts/interfaces/IERC20.sol
+++ contracts/interfaces/IERC20.sol
@@ -2,7 +2,6 @@ interface IERC20 {
 
     function totalSupply() external view returns (uint256);
 
-
     function balanceOf(address account) external view returns (uint256);
 
 
@@ -23,7 +22,6 @@ interface IERC20 {
 
 
     event Transfer(address indexed from, address indexed to, uint256 value);
-
 
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
