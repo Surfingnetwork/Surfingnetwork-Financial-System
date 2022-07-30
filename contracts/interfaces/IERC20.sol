9122131be4794ccafabb79ec3c571afddf735856
Surfingnetwork <103472119+Surfingnetwork@users.noreply.github.com>
2022-07-30 16:27:31 +0800

    Update IERC20.sol

--- contracts/interfaces/IERC20.sol
+++ contracts/interfaces/IERC20.sol
@@ -4,7 +4,6 @@ interface IERC20 {
 
     function balanceOf(address account) external view returns (uint256);
 
-
     function transfer(address recipient, uint256 amount) external returns (bool);
 
 
